535cd0f32928726183f8bbfe0ac4d64c78def015
Maria Yakubova <mariaaliceyakubova@gmail.com>
2021-09-15 14:02:29 -0500

    Final Code commit

--- src/test/java/com/paylocity/runners/CukesRunner.java
+++ src/test/java/com/paylocity/runners/CukesRunner.java
@@ -6,6 +6,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+
+
         features="src/test/resources/features",
         glue = "com/paylocity/step_definitions",
         dryRun = false,
@@ -17,3 +19,14 @@ import org.junit.runner.RunWith;
 
 public class CukesRunner {
 }
+
+
+
+
+/*
+plugin = {
+                "html:target/cucumber-report.html",
+                "json:target/cucumber.json",
+                "rerun:target/rerun.txt"
+        },
+ */
